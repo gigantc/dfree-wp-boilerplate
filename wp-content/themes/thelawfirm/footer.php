0378b4bee408a4456e944bcf047090cef6b05362
JFish <jfishman@kitchensinkstudios.com>
2016-01-04 11:10:26 -0700

    -- ready for use i think...

---
+++ wp-content/themes/thelawfirm/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package thelawfirm
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'thelawfirm' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'thelawfirm' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'thelawfirm' ), 'thelawfirm', '<a href="http://thelawfirm.kssdev.com" rel="designer">The Law Firm, Your Welcome.</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
