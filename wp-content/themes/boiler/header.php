4583f04b10a5f9ff754f6418d55401b2fd8d14db
dfree <dan@gigantc.com>
2019-09-04 15:22:56 -0700

    adding global roboto as the default front-end font

--- wp-content/themes/boiler/header.php
+++ wp-content/themes/boiler/header.php
@@ -11,10 +11,15 @@
 
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> id="bdhwk">
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+
+
 
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
