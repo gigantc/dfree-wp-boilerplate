2b8df751e6c97f5b54a58e780e2e8964ed2c0762
dfree <dan@gigantc.com>
2025-04-08 13:07:24 -0700

    cleaning up gulpfile

--- wp-content/themes/boiler/gulpfile.js
+++ wp-content/themes/boiler/gulpfile.js
@@ -1,87 +1,106 @@
 const { src, dest, watch, series, parallel } = require('gulp');
-
-/* PLUGINS */
 const sass = require('gulp-sass')(require('sass')),
-    browserSync = require("browser-sync").create(),
-    postcss = require("gulp-postcss"),
+    browserSync = require('browser-sync').create(),
+    postcss = require('gulp-postcss'),
     sourcemaps = require('gulp-sourcemaps'),
     babel = require('gulp-babel'),
-    cssnano = require("cssnano"),
-    autoprefixer = require("autoprefixer"),
+    cssnano = require('cssnano'),
+    autoprefixer = require('autoprefixer'),
     concat = require('gulp-concat'),
     terser = require('gulp-terser'),
-    plumber = require('gulp-plumber');
+    plumber = require('gulp-plumber'),
+    del = require('del');
 
-/* FILE PATHS */
-const files = { 
-    scssPath: ['src/scss/*.scss'],
-    scriptsPath: 'src/js/*.js',
-    libsPath: 'src/js/libs/*.js',
-    blocksPath: 'blocks/**/*.scss',
+
+
+//////////////////////////////////////
+// FILE PATHS
+const files = {
+  scssPath: ['src/scss/**/*.scss', 'blocks/**/*.scss'],
+  scriptsPath: 'src/js/*.js',
+  libsPath: 'src/js/libs/*.js'
 };
 
-/* STYLES TASK */
+
+//////////////////////////////////////
+// BABEL CONFIG
+const babelConfig = {
+  presets: ['@babel/env']
+};
+/* CLEAN TASK */
+function clean() {
+  return del(['css/*', 'js/*']);
+}
+
+
+//////////////////////////////////////
+// STYLES TASK
 function scssTask() {
   return src('src/scss/main.scss', { sourcemaps: true })
     .pipe(plumber())
     .pipe(sass({ includePaths: ['src/scss'] }))
     .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(dest('css', { sourcemaps: '.' }));
+    .pipe(dest('css', { sourcemaps: '.' }))
+    .pipe(browserSync.stream());
 }
 
-/* LIBS TASK */
-function libsTask() {    
-    return src(files.libsPath)
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(concat('libs.min.js'))
-        .pipe(terser())
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('js/libs/'));
+
+//////////////////////////////////////
+// LIBS TASK
+function libsTask() {
+  return src(files.libsPath)
+    .pipe(sourcemaps.init())
+    .pipe(babel(babelConfig))
+    .pipe(concat('libs.min.js'))
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('js/libs/'));
 }
 
-/* SCRIPTS TASK */
-function scriptsTask() {    
-    return src(files.scriptsPath)
-        .pipe(plumber({
-            errorHandler: function (error) {
-                console.log(error.message);
-                this.emit('end');
-            }
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(concat('main.min.js'))
-        .pipe(terser())
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('js/'))
-        .pipe(browserSync.stream());
+
+//////////////////////////////////////
+// SCRIPTS TASK
+function scriptsTask() {
+  return src(files.scriptsPath)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(babel(babelConfig))
+    .pipe(concat('main.min.js'))
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('js/'))
+    .pipe(browserSync.stream());
 }
 
-/* BROWSER SYNC TASK */
+
+//////////////////////////////////////
+// BROWSER SYNC RELOAD
 function reload(done) {
-    browserSync.reload();
-    done();
+  browserSync.reload();
+  done();
 }
 
-/* WATCH TASK */
+
+//////////////////////////////////////
+// WATCH TASK
 function watchTask() {
-    browserSync.init({
-        proxy: "boiler.local"
-    });
-    watch(files.blocksPath, scssTask);
-    watch(files.scssPath, scssTask);
-    watch(files.libsPath, libsTask);
-    watch(files.scriptsPath, scriptsTask);
-    watch([files.scssPath, files.scriptsPath], reload);
+  browserSync.init({
+    proxy: 'dfreeboilerplate.local'
+  });
+  watch(files.scssPath, scssTask);
+  watch(files.libsPath, libsTask);
+  watch(files.scriptsPath, scriptsTask);
+  watch('**/*.php', reload); // Reload when PHP files change
 }
 
-/* DEFAULT TASK */
+//////////////////////////////////////
+// EXPORT TASKS
 exports.default = series(
-    parallel(scssTask, libsTask, scriptsTask), 
-    watchTask
+  parallel(scssTask, libsTask, scriptsTask),
+  watchTask
+);
+
+exports.build = series(
+  clean,
+  parallel(scssTask, libsTask, scriptsTask)
 );
